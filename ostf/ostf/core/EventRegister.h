0ca490872926b86f2494f25da57eb7fcd39ea8e8
Charlie Gillies <charlie@hazardinteractive.com>
2017-08-01 19:10:42 +0100

    ensures two events with the same id can't be declared

--- ostf/ostf/core/EventRegister.h
+++ ostf/ostf/core/EventRegister.h
@@ -47,15 +47,45 @@ namespace ostf
 			}
 		}
 
+		/*
+			Declares an event type to the register.
+		*/
 		template<class E>
 		void declare() {
 			// ensure that 'E' inherits from Event
 			static_assert(std::is_base_of<Event, E>::value, 
 				"EventRegister.declare<E>() can only take template types that inherit from ostf::Event.");
 
-			// create the generator and pop it into the map
-			//EventGenerator<E>* generator = new EventGenerator<E>();
+			// get the key of the event
+			std::string key = E::id;
 
+			// ensure there is no current generator with this id
+			// this might mean that the same event has been declared twice
+			// or it might mean that two events have been given the same id
+			BaseEventGenerator* currentGenerator = _generators[key];
+			if (currentGenerator != nullptr) {
+				throw std::invalid_argument("The events id propert has already been declared");
+			}
+
+			// create the generator and pop it into the map
+			_generators[key] = new EventGenerator<E>();
+		}
+
+		/*
+			Attempts to create an event from a given id.
+			If the id is not recognised or cannot be generated
+			then nullptr will be returned.
+		*/
+		Event* generate(ostf::Buffer& buffer) {
+			// get the base event generator using the id
+			std::string id;
+			buffer.read_string("id", id);
+			BaseEventGenerator* generator = _generators[id];
+		
+			if (generator == nullptr)
+				return nullptr;
+
+			return generator->generate(buffer);
 		}
 	};
 }
